98108642840d1af720dc32ef4a0fe8c1cf3272ad
Cui SHuhan <shuhan.cui@student.manchester.ac.uk>
2026-02-11 00:19:54 +0800

    Added hello_world script

---
+++ lab-a/hello_world.ps1
@@ -0,0 +1,24 @@
+# Display a welcome message
+$name = "Shuhan"
+Write-Host "Hello $name!"
+
+# Display a different message depending on the value of $hour
+$hour = 13
+if ($hour -ge 12) {
+    Write-Host "It is the afternoon."
+} else {
+    Write-Host "It is the morning."
+}
+
+# Display colours of the rainbow
+$colors = @("Red","Orange","Yellow","Green","Blue","Indigo","Violet")
+For ($i=0; $i -lt $colors.Length; $i++) {
+    Write-Host $colors[$i]
+}
+
+# Display the contents of /
+Get-ChildItem -Path / | ForEach-Object {
+    Write-Host "$_"
+}
+git config --global user.name "Cui SHuhan"
+git config --global user.email "shuhan.cui@student.manchester.ac.uk"
